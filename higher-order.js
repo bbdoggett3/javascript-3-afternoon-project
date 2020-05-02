9f5cca8291e749474fd82308b5e574e644cb3c6c
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-01 18:52:08 -0600

    I just finished higher-order

--- higher-order.js
+++ higher-order.js
@@ -47,7 +47,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map(element => {
+  const arr = element * 1.07;
+  return arr;
+});
 
 
 
@@ -66,7 +69,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce((acc, element) => {
+  return acc + element;
+}, 0)
+
+
 
 
 
@@ -92,7 +99,11 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest  = monstersInYourPocket.filter(element => {
+  if(element['CP'] > 200) {
+    return element
+  }
+}, "The monsters are")
 
 
 
@@ -109,7 +120,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(element => {
+  return element['price'] * (1 + element['tax']);
+})
 
 
 
@@ -129,6 +142,11 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((total, element) => {
+  if(element.owner === 'Bob') {
+    total += element.price;
+  }
+  return total;
+}, 0)
 
 
